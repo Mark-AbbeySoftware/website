ab1426f60c346ea04ee4033732751a84bc4d853e
Mark Gregory <mark.gregory@mpba.co.uk>
2025-06-02 09:31:43 +0100

    Added Redis

--- deploy.php
+++ deploy.php
@@ -7,35 +7,34 @@ require 'recipe/laravel.php';
 
 // Config
 set('application', 'Laravel Abbey Software Development Web Application');        // The Application Title
-set('repository', 'git@abbeysoftware.github.com:Mark-AbbeySoftware/vcms.git');   // SCM Target
-set('keep_releases',
-    2);                                                           // Number of releases to keep on hosts
-set('default_timeout', 1200);                                                      // default ssh timeout
+set('repository', 'git@absdev.github.com:Mark-AbbeySoftware/website.git');       // SCM Target
+set('keep_releases', 2);                                                         // Number of releases to keep on hosts
+set('default_timeout', 1200);                                                    // default ssh timeout
 
-add('shared_files', ['.env']);                                                             // shared files
+add('shared_files', ['.env']);                                             // shared files
 add('shared_dirs', ['storage', 'vendor', 'bootstrap/cache']);              // Shared dirs between deploys
 add('writable_dirs', ['storage', 'vendor', 'bootstrap/cache']);            // Writable dirs by web server
 
 
-set('bin/php', function () {
-    return '/usr/bin/php7.4';
-});
+//set('bin/php', function () {
+//    return '/usr/bin/php7.4';
+//});
 
 // Core Tasks
 
-task('npm:install', function () {
-    run("cd {{release_path}} && /usr/bin/npm install");
-})->desc('Running npm install');
+//task('npm:install', function () {
+//    run("cd {{release_path}} && /usr/bin/npm install");
+//})->desc('Running npm install');
 
-task('npm:build', function () {
-    run("cd {{release_path}} && /usr/bin/npm run build");
-})->desc('Running npm build');
+//task('npm:build', function () {
+//    run("cd {{release_path}} && /usr/bin/npm run build");
+//})->desc('Running npm build');
 
-task('build', function () {
-    #cd('{{release_path}}');
-    #run('/usr/bin/npm install');
-    #run('/usr/bin/npm run build');
-});
+//task('build', function () {
+#cd('{{release_path}}');
+#run('/usr/bin/npm install');
+#run('/usr/bin/npm run build');
+//});
 
 // Hosts
 
@@ -55,10 +54,9 @@ host('prod')
 
 // Hooks
 
-after('deploy:update_code', 'build');
+//after('deploy:update_code', 'build');
 after('deploy:success', 'artisan:config:clear');
 after('deploy:success', 'artisan:route:clear');
 after('deploy:success', 'artisan:cache:clear');
-
 after('deploy:failed', 'deploy:unlock');
 
